4389953cf1c7b13c5f8cbbef3c5daf9cdb0ba1f6
Divij Kardile <kardiledivij@gmail.com>
2021-01-03 00:50:05 +0530

    half done

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOZOO-aD-iAIgt7W3OBnPZigu8C9DYtdk",
+  authDomain: "whatsapp-clone-ed4ab.firebaseapp.com",
+  projectId: "whatsapp-clone-ed4ab",
+  storageBucket: "whatsapp-clone-ed4ab.appspot.com",
+  messagingSenderId: "976627653327",
+  appId: "1:976627653327:web:359788d5226c736e76f5ba",
+  measurementId: "G-XY1V6376WJ",
+};
